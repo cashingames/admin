f5d91446ca39e1c9085f4ab1be2a9fd17a778101
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-03-06 14:42:48 +0400

    small fixes

--- resources/views/gaming/sessions.blade.php
+++ resources/views/gaming/sessions.blade.php
@@ -3,7 +3,7 @@
             name="all-users"
             model="App\Models\Live\GameSession"
             with="user, category, mode, plan"
-            include="id,user.username,category.name|sub-category,mode.name|game mode,plan.name,points_gained|points,state,start_time,end_time"
+            include="id,user.username,category.name|sub-category,mode.name|mode,plan.name|plan,points_gained|points,state,start_time,end_time"
             sort="start_time|desc"
             searchable="user.username"
             hide=""
