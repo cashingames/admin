286dbbabc760e7431237b5b27ef656a06efa02a3
Zubby <zubbybrightson@gmail.com>
2022-03-10 15:53:29 +0100

    display question detail in modal

--- app/Http/Livewire/QuestionsTableModal.php
+++ app/Http/Livewire/QuestionsTableModal.php
@@ -10,17 +10,17 @@ class QuestionsTableModal extends ModalComponent
 
     public $question;
 
-    public function mount(Question $question)
+    public function mount($id)
     {
-        $this->question = $question;
+        $this->question = Question::find($id);
     }
 
     public function render()
     {
-        return view('livewire.questions-table-modal');
+        // return view('livewire.questions-table-modal');
 
         return view('livewire.questions-table-modal', [
-            'questions' => Question::all(),
+            'question' =>$this->question,
         ]);
     }
 }
--- resources/views/livewire/questions-table-modal.blade.php
+++ resources/views/livewire/questions-table-modal.blade.php
@@ -1,15 +1,25 @@
 <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+            <h5 class="modal-title" wire:model="question">{{$question->id}}</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
-            <p>Modal body text goes here.</p>
+        <div class="modal-header">
+            <h5 class="modal-title" wire:model="question">{{$question->label}}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="modal-header">
+            <h5 class="modal-title" wire:model="question">{{$question->level}}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-header">
+            <h5 class="modal-title" wire:model="question">{{$question->category->name}}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-header">
+                @foreach ($question->options as $option)
+                    <h5 class="modal-title" wire:model="question">{{$option->title}}</h5>
+                @endforeach
         </div>
     </div>
 </div>
