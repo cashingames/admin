6fdfd66e3dd2f93838baa536d67ea94e25d6127c
Zubby <zubbybrightson@gmail.com>
2022-04-11 20:21:35 +0100

    add a comment functionality

--- resources/views/components/table-actions.blade.php
+++ resources/views/components/table-actions.blade.php
@@ -5,4 +5,13 @@
                 <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path></svg>
         </button>
     </a>
+    @can('super-admin-access')
+    <button class="p-4 text-teal-600 hover:bg-teal-600 hover:text-white rounded" onclick='Livewire.emit("openModal", "modals.add-comment", {{ json_encode(["id" => $id]) }})'>
+         +Add a comment
+    </button>
+    @elsecan('content-admin-access')
+    <button class="p-4 text-teal-600 hover:bg-teal-600 hover:text-white rounded" onclick='Livewire.emit("openModal", "modals.add-comment", {{ json_encode(["id" => $id]) }})'>
+         +Add a comment
+    </button>
+    @endcan
 </div>
