a44c966158bc74355890617f84b3c9f187773d77
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-05-08 12:01:55 +0400

    adding search by names

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -7,7 +7,7 @@
             with="profile"
             include="id, username, email, profile.first_name, profile.last_name, phone_number, profile.gender, profile.state, profile.referral_code, profile.account_name, profile.account_number, profile.bank_name, profile.referrer, created_at"
             sort="created_at|desc"
-            searchable="username, email, phone_number, profile.referrer"
+            searchable="profile.first_name, profile.last_name, username, email, phone_number, profile.referrer"
             hide="profile.gender, profile.state, profile.referral_code, profile.account_name, profile.account_number, profile.bank_name"
             hideable="select"
             exportable
