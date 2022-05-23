c2fa4b67f572ea250bf36051f5a512de92cc8796
Zubby <zubbybrightson@gmail.com>
2022-05-23 20:08:18 +0100

    wip add questions dynamically :> add dynamic question selection

--- app/Http/Livewire/Modals/AddTrivia.php
+++ app/Http/Livewire/Modals/AddTrivia.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Livewire\Modals;
+
 use App\Models\Live\Trivia;
 use App\Models\Live\Category;
+use App\Models\Live\Question;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Carbon;
 
@@ -12,17 +14,21 @@ use LivewireUI\Modal\ModalComponent;
 class AddTrivia extends ModalComponent
 {
 
-    public $trivia, $subcategories, $name, $grand_price, $points_required;
-    public $game_duration,$question_count,$start_time,$end_time,$subcategory;
-   
+    public $trivia, $subcategories, $name, $grand_price, $points_required, $questions ;
+    public $game_duration, $question_count, $start_time, $end_time, $subcategory, $selectedQuestion;
+    public $canChooseQuestions = false;
+    public $notifyError = false;
+    public $selectedQuestions = [];
+
     public function mount()
-    {  
-        $this->subcategories = Category::where('category_id' ,'>', 0)->get();
+    {   
+        $this->value = 2;
+        $this->subcategories = Category::where('category_id', '>', 0)->get();
     }
 
     public function addTrivia()
-    {  
-        $response = Http::acceptJson()->post(config('app.api_url').'/v3/trivia/create', [
+    {
+        $response = Http::acceptJson()->post(config('app.api_url') . '/v3/trivia/create', [
             'name' => $this->name,
             'category' => $this->subcategory,
             'grand_price' => $this->grand_price,
@@ -32,22 +38,58 @@ class AddTrivia extends ModalComponent
             'game_duration' => $this->game_duration,
             'question_count' => $this->question_count
         ]);
-      
 
-        if($response->successful()){
+
+        if ($response->successful()) {
             return redirect()->to('/gaming/trivia');
-     
         }
-        if($response->failed()){
+        if ($response->failed()) {
             $response->throw();
         }
-       
     }
+
+    public function updatedSubcategory()
+    {
+        if( $this->notifyError ){
+            $this->notifyError = false;
+        }
+    }
+
+    public function toggleCanChooseQuestions()
+    {   
+        if ($this->canChooseQuestions) {
+            $this->canChooseQuestions = false;
+        } else {
+            $category = Category::where('name', $this->subcategory)->first();
+            if($category === null){
+                $this->notifyError = true;
+                $this->canChooseQuestions = false;
+            }else{
+                $this->canChooseQuestions = true;
+                $this->notifyError = false;
+                $this->questions = Question::where('category_id', $category->id)
+                ->inRandomOrder()->limit(100)->get();
+            }
+        }
+    }
+
+    public function addToSelectedQuestions()
+    {
+        array_push($this->selectedQuestions, $this->selectedQuestion);
+    }
+
+    public function removeFromSelectedQuestions($value)
+    {   
+    
+        $key = array_search($value, $this->selectedQuestions, true);
+        if ($key !== false) {
+            array_splice($this->selectedQuestions, $key, 1);
+        }
+    }
+
 
     public function render()
     {
         return view('livewire.modals.add-trivia');
     }
-
- 
 }
--- resources/views/livewire/modals/add-trivia.blade.php
+++ resources/views/livewire/modals/add-trivia.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="w-full max-w-lg pt-6" >
-    
+    <div class="w-full max-w-lg pt-6">
+
         <div class="mb-6">
             <div class="w-full  px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
@@ -8,7 +8,7 @@
                 </label>
                 <textarea rows="1" , cols="54" wire:model="name"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                    type="text" placeholder="Trivia Name" ></textarea>
+                    type="text" placeholder="Trivia Name"></textarea>
             </div>
             <div class="w-full  px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
@@ -24,8 +24,8 @@
                 </label>
                 <select
                     class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   wire:model="subcategory">
-                   <option>select subcategory</option>
+                    wire:model="subcategory">
+                    <option>select subcategory</option>
                     @foreach ($subcategories as $s)
                     <option>{{$s->name}}</option>
                     @endforeach
@@ -45,7 +45,7 @@
                 </label>
                 <input wire:model="question_count" type="number"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                   placeholder="Number of Questions">
+                    placeholder="Number of Questions">
             </div>
             <div class="w-full  px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
@@ -53,8 +53,8 @@
                 </label>
                 <select
                     class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   wire:model="game_duration">
-                   <option>select duration</option>
+                    wire:model="game_duration">
+                    <option>select duration</option>
                     <option>60</option>
                     <option>120</option>
                     <option>180</option>
@@ -65,11 +65,11 @@
         </div>
         <div class="w-full  px-3 mb-6 md:mb-0">
             <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
-               Start Time
+                Start Time
             </label>
             <input wire:model="start_time" type="datetime-local"
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              placeholder="Start Time" >
+                placeholder="Start Time">
         </div>
         <div class="w-full  px-3 mb-6 md:mb-0">
             <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
@@ -77,14 +77,55 @@
             </label>
             <input wire:model="end_time" type="datetime-local"
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                placeholder="End Time"  >
+                placeholder="End Time">
         </div>
+        <div class="w-full  px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                Choose Questions?
+            </label>
+            <input wire:click="toggleCanChooseQuestions" type="checkbox">
+        </div>
+        @if ($notifyError)
+        <span class="block uppercase tracking-wide text-red-500 text-center text-xs font-bold mb-2"> Please select a
+            subcategory before adding questions</span>
+        @endif
+        @if ($canChooseQuestions)
+        <div class="w-full  px-3 mb-6 md:mb-0">
+            <div class="w-full  px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                    Selected Questions will show below:
+                </label>
+                <ol>
+                    @foreach ($selectedQuestions as $i=>$q)
+                    <div class="flex flex-row">
+                    <li>{{$i+1}} {{$q}}</li>
+                    <button wire:click="removeFromSelectedQuestions({{ $q }})" class="shadow text-red-500 bg-white px-1 ml-1 mt-1 rounded">
+                        x
+                    </button>
+                    </div>
+
+                    @endforeach
+                </ol>
+            </div>
+            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                Eligible Questions
+            </label>
+            <select
+                class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                wire:model="selectedQuestion">
+                <option>select question</option>
+                @foreach ($questions as $q)
+                <option wire:click="addToSelectedQuestions">{{$q->label}}</option>
+                @endforeach
+            </select>
+        </div>
+        @endif
         <div class="md:items-center mb-4">
-           
-            <button wire:click="addTrivia" wire:loading.attr="disabled" class="shadow bg-blue-500 text-white font-bold ml-4 py-2 px-4 rounded">
+            <button wire:click="addTrivia" wire:loading.attr="disabled"
+                class="shadow bg-blue-500 text-white font-bold ml-4 py-2 px-4 rounded">
                 Save
             </button>
-           
-    </div>
 
-</div>
+        </div>
+
+    </div>
