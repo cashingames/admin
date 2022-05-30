9a7794a683b8102d6243eb069bc1b5e06595e439
Zubby <zubbybrightson@gmail.com>
2022-05-30 18:08:41 +0100

    fix export bug. change to class based method

---
+++ app/Http/Livewire/Players/UsersTable.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Livewire\Players;
+
+
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+use App\Models\Live\User;
+use App\Models\Live\Profile;
+
+class UsersTable extends LivewireDatatable
+{
+
+    public function builder()
+    {
+
+        return User::query();
+    }
+
+
+    public function columns()
+    {
+        return
+            [
+                NumberColumn::name('id')
+                    ->label('ID'),
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->first_name;
+                }, 'first_name')->label('First Name'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->last_name;
+                }, 'last_name')->label('Last Name'),
+
+                Column::name('username')
+                    ->searchable()
+                    ->filterable(),
+
+                Column::name('email')
+                    ->label('Email')
+                    ->filterable()
+                    ->searchable(),
+
+                Column::name('phone_number')
+                    ->label('Phone Number')
+                    ->filterable()
+                    ->searchable(),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->gender;
+                }, 'gender')->label('Gender'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->referral_code;
+                }, 'referral_code')->label('Referral Code'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->account_name;
+                }, 'account_name')->label('Account Name'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->bank_name;
+                }, 'bank_name')->label('Bank Name'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->account_number;
+                }, 'account_number')->label('Account Number'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->state;
+                }, 'state')->label('State'),
+
+                Column::callback(['id'], function ($id) {
+                    $profile = Profile::where('user_id', $id)->first();
+                    return $profile->referrer;
+                }, 'referrer')->label('Referrer'),
+
+                Column::name('created_at')
+                    ->searchable()
+                    ->filterable()
+
+            ];
+    }
+}
--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -1,21 +1,9 @@
 <x-customers-layout>
     @can('super-admin-access')
-   
-        <livewire:datatable 
-            name="all-users"
-            model="App\Models\Live\User"
-            with="profile"
-            include="id, username, email, profile.first_name, profile.last_name, phone_number, profile.gender, profile.state, profile.referral_code, profile.account_name, profile.account_number, profile.bank_name, profile.referrer, created_at"
-            sort="created_at|desc"
-            searchable="profile.first_name, profile.last_name, username, email, phone_number, profile.referrer"
-            hide="profile.gender, profile.state, profile.referral_code, profile.account_name, profile.account_number, profile.bank_name"
-            hideable="select"
-            exportable
-        />
-   
+    <livewire:players.users-table exportable hideable="select" />
     @else
-        You are not authorised to access this data.
-   
+    You are not authorised to access this data.
+
     @endcan
-        
+
 </x-customers-layout>
