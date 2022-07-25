d04fd904343a57d81b900d001b7bce710a57e8b7
Zubby <zubbybrightson@gmail.com>
2022-07-25 11:02:57 +0100

    make users with no games exportable

--- resources/views/customers/reportsData.blade.php
+++ resources/views/customers/reportsData.blade.php
@@ -2,5 +2,5 @@
     <div class=" text-center text-gray-600 text-lg font-bold m-2">
         <span>Registered Users Yet to Play A Game</span>
     </div>
-    <livewire:players.users-with-no-games />
+    <livewire:players.users-with-no-games exportable hideable="select" />
 </x-customers-layout>
