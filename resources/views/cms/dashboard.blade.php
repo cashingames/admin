5f1d639793ca86936339d10bd79ab6f270c38d45
Zubby Uwajimgba <zubbybrightson@gmail.com>
2022-11-17 19:43:06 +0100

    wip

--- resources/views/cms/dashboard.blade.php
+++ resources/views/cms/dashboard.blade.php
@@ -1,7 +1,4 @@
 <x-cms-layout>
   <livewire:cms.reports />
-  <div class="mt-4">
-    <p class=" justify-center text-center text-gray-600 text-lg font-bold m-8">QUESTION-CATEGORY STATS</p>
-    <livewire:cms.questions-category-statistics hideable='select' exportable />
-  </div>
+  
 </x-cms-layout>
