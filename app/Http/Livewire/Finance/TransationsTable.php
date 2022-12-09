96016059146c8be0784f9d9dcdfad5d29e9027a5
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-12-09 09:22:07 +0400

    improvements to transaction table

--- app/Http/Livewire/Finance/TransationsTable.php
+++ app/Http/Livewire/Finance/TransationsTable.php
@@ -15,8 +15,10 @@ class TransationsTable extends LivewireDatatable
 {
     public $exportable = true;
     public $hideable = 'select';
-    public $hide = ['balance'];
     public $complex = true;
+    public $persistPerPage = false;
+    public $persistHiddenColumns = false;
+    public $defaultSortColumn = 'created_at';
     public $searchable = [
         'users.email', 'users.username', 'users.phone_number', 'reference', 'description',
         'users.phone_number'
@@ -24,7 +26,6 @@ class TransationsTable extends LivewireDatatable
     public $groupLabels = [
          'user' => 'TOGGLE USER DETAILS'
     ];
-    public $defaultSortColumn = 'created_at';
 
     public function builder()
     {
@@ -38,39 +39,35 @@ class TransationsTable extends LivewireDatatable
         return
             [
                 Column::index($this),
-                
-                Column::name('users.username')->filterable()->group('user'),
-
-                Column::name('users.email')->group('user'),
-
-                Column::name('users.phone_number')->group('user'),
-
-                Column::name('reference'),
-
-                Column::name('description')->filterable([
-                    'Fund Wallet',
-                    'Sign Up Bonus',
-                    'Winnings Withdrawal Made',
-                    'Placed a staking',
-                    'Bought TIME FREEZE boosts',
-                    'Bought BOOST boosts',
-                    'Bought BOMB boosts',
-                ])->searchable(),
-
-                Column::name('transaction_type')->filterable(['CREDIT', 'DEBIT']),
-
-                NumberColumn::callback(['amount', 'transaction_type'], function ($amount, $transactionType) {
-                    return $transactionType == 'CREDIT' ? $amount : -$amount;
-                })->label('Amount')->filterable()->enableSummary(),
-
-                NumberColumn::name('balance')->hide(),
-
                 DateColumn::callback('created_at', function ($createdAt) {
                     return Carbon::parse($createdAt)->setTimezone('Africa/Lagos');
                 })
                 ->label("Transaction Date")
                 ->filterable(),
 
+                Column::name('reference')->hide(),
+
+                Column::name('description')->filterable([
+                    'Fund Wallet',
+                    'Sign Up Bonus',
+                    'Winnings Withdrawal Made',
+                    'Placed a staking'
+                ]),
+
+                Column::name('transaction_type')->filterable(['CREDIT', 'DEBIT'])->hide(),
+
+                NumberColumn::callback(['amount', 'transaction_type'], function ($amount, $transactionType) {
+                    return $transactionType == 'CREDIT' ? $amount : -$amount;
+                })->label('Amount')->filterable()->enableSummary(),
+
+                NumberColumn::name('balance'),
+
+                //@TODO Hide a group
+                Column::name('users.username')->filterable()->group('user'),
+
+                Column::name('users.email')->group('user'),
+
+                Column::name('users.phone_number')->group('user'),
             ];
     }
 }
