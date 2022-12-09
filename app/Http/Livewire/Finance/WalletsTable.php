89e81e86456d82aaea9be7c58087c2214c45fc48
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-12-09 15:05:18 +0400

    quick changes

--- app/Http/Livewire/Finance/WalletsTable.php
+++ app/Http/Livewire/Finance/WalletsTable.php
@@ -15,9 +15,7 @@ class WalletsTable extends LivewireDatatable
     public $hideable = 'select';
     public $complex = true;
     public $persistPerPage = false;
-    public $perPage = 100;
     public $persistHiddenColumns = false;
-    public $defaultSortColumn = 'created_at';
     public $searchable = [
         'users.email', 'users.username', 'users.phone_number'
     ];
@@ -39,21 +37,21 @@ class WalletsTable extends LivewireDatatable
 
                 Column::name('users.username')->filterable()->group('user'),
 
-                Column::name('users.email')->group('user'),
+                Column::name('users.email')->group('user')->hide(),
 
-                Column::name('users.phone_number')->group('user'),
+                Column::name('users.phone_number')->group('user')->hide(),
 
-                NumberColumn::name('non_withdrawable_balance')->label('Platform Balance'),
+                NumberColumn::name('non_withdrawable_balance')->label('Inflow Cash')->filterable()->enableSummary(),
 
-                NumberColumn::name('withdrawable_balance')->label('Earnings Balance'),
-
-                DateColumn::callback('created_at', function ($createdAt) {
-                    return Carbon::parse($createdAt)->setTimezone('Africa/Lagos');
-                })->filterable(),
+                NumberColumn::name('withdrawable_balance')->label('Outflow Cash')->filterable()->enableSummary(),
 
                 DateColumn::callback('updated_at', function ($updatedAt) {
                     return Carbon::parse($updatedAt)->setTimezone('Africa/Lagos');
-                })->filterable(),
+                })->label('Last Activity')->filterable(),
+
+                DateColumn::callback('created_at', function ($createdAt) {
+                    return Carbon::parse($createdAt)->setTimezone('Africa/Lagos');
+                })->label('Joined')->filterable(),
 
 
             ];
