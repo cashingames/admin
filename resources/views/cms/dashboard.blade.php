af42b8f80853a37d2a0678f3618b855e734c188c
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-01-09 18:24:48 +0100

    modify permission

--- resources/views/cms/dashboard.blade.php
+++ resources/views/cms/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-cms-layout>
-  @if (Auth::user()->hasTeamPermission(Auth::user()->currentTeam, 'cms:dashboard') )
+  @if (Auth::user()->hasTeamPermission(Auth::user()->currentTeam, 'cms:view') )
   <livewire:cms.reports />
   @else
   <p>You are not authorized to access this data</p>
