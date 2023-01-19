beaf84f6e2d055e7d1114f3f267ca328814f7ed5
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-01-19 11:33:39 +0100

    prize pool details

--- resources/views/livewire/gaming/add-trivia.blade.php
+++ resources/views/livewire/gaming/add-trivia.blade.php
@@ -75,7 +75,7 @@
                 <div class="w-full  px-3 mb-6 md:mb-0">
 
                     <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
-                       Entry Mode
+                        Entry Mode
                     </label>
                     <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="entryMode">
                         <option>select entry mode</option>
@@ -105,33 +105,20 @@
             <div class="w-full flex flex-row justify-center ">
                 <span class="block uppercase tracking-wide mt-4 text-gray-700 text-l font-bold mb-2">Prize Pool</span>
             </div>
-            <div class="w-full flex flex-row justify-center ">
-                <div class="mb-6">
-                    <div class="w-full  px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
-                            Position From
-                        </label>
-                        <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
-                            <option>select position</option>
-                            @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
-                                @endfor
-                        </select>
-                    </div>
-                    <div class="w-full  px-3 mb-6 md:mb-0">
 
-                        <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
-                            Prize Type
-                        </label>
-                        <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="game_duration">
-                            <option>select prize type</option>
-                            @foreach ($prizeTypes as $prizeType)
-                            <option>{{$prizeType}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="mb-6">
-                    <div class="w-full  px-3 mb-6 md:mb-0">
+            <div class="w-full flex flex-row justify-between">
+                <div>
+                    <div class="mb-6">
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Position From
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
+                                <option>select position</option>
+                                @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
+                                    @endfor
+                            </select>
+                        </div>
                         <div class="w-full  px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
                                 Position To
@@ -141,42 +128,148 @@
                                 @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
                                     @endfor
                             </select>
+
+                        </div>
+                    </div>
+                    <div class="mb-6">
+
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Prize Type
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="game_duration">
+                                <option>select prize type</option>
+                                @foreach ($prizeTypes as $prizeType)
+                                <option>{{$prizeType}}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
                             Prize
                         </label>
                         <input wire:model="prize" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" placeholder="prize">
+
+
                     </div>
-
                 </div>
+                <div>
+                    <div class="mb-6">
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Position From
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
+                                <option>select position</option>
+                                @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
+                                    @endfor
+                            </select>
+                        </div>
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Position To
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
+                                <option>select position</option>
+                                @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
+                                    @endfor
+                            </select>
 
-            </div>
-            <div class="flex flex-row w-full  px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2" checked>
-                    Do you want to Manually add questions?
-                </label>
-                @if (!$canChooseQuestions)
-                <button wire:click="toggleCanChooseQuestions" class="shadow mt-2 mb-2 ml-2 bg-blue-500 text-white font-bold px-4 rounded">
-                    yes
-                </button>
-                @else
-                <button wire:click="toggleCanChooseQuestions" class="shadow mt-2 mb-2 ml-2 bg-blue-500 text-white font-bold px-4 rounded">
-                    no
-                </button>
-                @endif
-            </div>
-            <div class="w-full flex flex-column justify-center">
-                @if ($canChooseQuestions)
-                <div class="flex flex-column justify-center mt-6 w-full  px-3 mb-6 md:mb-0">
-                    <livewire:cms.select-questions-table>
-                </div>
-                @else
-                <div class="md:items-center mb-4">
-                    <button wire:click="addTrivia" wire:loading.attr="disabled" class="shadow bg-blue-500 text-white font-bold ml-4 py-2 px-4 rounded">
-                        Save
-                    </button>
-                </div>
-                @endif
 
+                        </div>
+                    </div>
+                    <div class="mb-6">
+
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Prize Type
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="game_duration">
+                                <option>select prize type</option>
+                                @foreach ($prizeTypes as $prizeType)
+                                <option>{{$prizeType}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                            Prize
+                        </label>
+                        <input wire:model="prize" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" placeholder="prize">
+
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-6">
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Position From
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
+                                <option>select position</option>
+                                @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
+                                    @endfor
+                            </select>
+                        </div>
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Position To
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="subcategory">
+                                <option>select position</option>
+                                @for($i = 0; $i < $numberOfWinners; $i++ ) <option>{{$i + 1}}</option>
+                                    @endfor
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="mb-6">
+
+                        <div class="w-full  px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                                Prize Type
+                            </label>
+                            <select class="block appearance-none w-full mb-4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" wire:model="game_duration">
+                                <option>select prize type</option>
+                                @foreach ($prizeTypes as $prizeType)
+                                <option>{{$prizeType}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2">
+                            Prize
+                        </label>
+                        <input wire:model="prize" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" placeholder="prize">
+
+                    </div>
+                </div>
             </div>
         </div>
+
+        <div class="flex flex-row w-full  px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide mt-4 text-gray-700 text-xs font-bold mb-2" checked>
+                Do you want to Manually add questions?
+            </label>
+            @if (!$canChooseQuestions)
+            <button wire:click="toggleCanChooseQuestions" class="shadow mt-2 mb-2 ml-2 bg-blue-500 text-white font-bold px-4 rounded">
+                yes
+            </button>
+            @else
+            <button wire:click="toggleCanChooseQuestions" class="shadow mt-2 mb-2 ml-2 bg-blue-500 text-white font-bold px-4 rounded">
+                no
+            </button>
+            @endif
+        </div>
+        <div class="w-full flex flex-column justify-center">
+            @if ($canChooseQuestions)
+            <div class="flex flex-column justify-center mt-6 w-full  px-3 mb-6 md:mb-0">
+                <livewire:cms.select-questions-table>
+            </div>
+            @else
+            <div class="md:items-center mb-4">
+                <button wire:click="addTrivia" wire:loading.attr="disabled" class="shadow bg-blue-500 text-white font-bold ml-4 py-2 px-4 rounded">
+                    Save
+                </button>
+            </div>
+            @endif
+
+        </div>
+    </div>
