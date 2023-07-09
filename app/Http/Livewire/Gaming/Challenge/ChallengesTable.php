d806077796c76e8dc47ab487d47674d863666ab8
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-07-09 18:58:21 +0100

    update challenges table

--- app/Http/Livewire/Gaming/Challenge/ChallengesTable.php
+++ app/Http/Livewire/Gaming/Challenge/ChallengesTable.php
@@ -6,9 +6,6 @@ use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 use Mediconesystems\LivewireDatatables\Column;
 use App\Models\Live\ChallengeRequest;
 use Illuminate\Support\Facades\DB;
-use Mediconesystems\LivewireDatatables\BooleanColumn;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Collection;
 
 class ChallengesTable extends LivewireDatatable
 {
@@ -24,55 +21,47 @@ class ChallengesTable extends LivewireDatatable
     public $groupLabels = [
         'opponent' => 'TOGGLE OPPONENT DETAILS'
     ];
-    public $searchable = [
-        'categories.name', 'request.request_username', 'request.request_email_address',
-        'request.request_phone_number', 'request.request_amount_staked', 'request.request_amount_won',
-        'opponent.username', 'user_op.phone_number', 'user_op.email', 'opponent.amount', 'opponent.score',
-        'opponent.amount_won', 'request.created_at'
-    ];
 
     public function builder()
     {
-        return ChallengeRequest::select('request.*', 'opponent.user_id', 'user_o.phone_number as opponent_phonenumber', 'user_o.email as opponent_email', 'user_o.username as opponet_username', 'opponent.score as opponet_score', 'opponent.amount_won as opponent_amount_won')
-            ->join(DB::raw('(select users.username as request_username, challenge_requests.created_at, session_token as request_session, amount as request_amount_staked, amount_won as request_amount_won, status as request_status, score as request_score, user_id as request_user_id, users.phone_number as request_phone_number, users.email as request_email_address, users.is_a_bot as request_bot, users.created_at as request_joined_on from challenge_requests, users where users.id = challenge_requests.user_id group by session_token) as request'), 'request.request_session', '=', 'challenge_requests.session_token')
-            ->join('users as user_o', 'user_o.id', '=', 'challenge_requests.user_id')
-            ->join('categories', 'categories.id', '=', 'challenge_requests.category_id')
-            ->join('challenge_requests as opponent', function ($join) {
-                $join->on('request.request_session', '=', 'opponent.session_token')
-                    ->on('request.request_user_id', '<>', 'opponent.user_id');
-            })->join('users as user_op', 'user_op.id', '=', 'opponent.user_id')->groupBy('request.request_session');
+        return ChallengeRequest::query()->select(
+            'challenge_requests.created_at',
+            'categories.name as category_name',
+            'challenge_requests.request_mode',
+            DB::raw('(CASE WHEN cr2.user_id = 1 then "BOT" else cr2.username end) player'),
+            'cr2.score as player_score',
+            DB::raw('(CASE WHEN challenge_requests.user_id = 1 then "BOT" else challenge_requests.username end) opponent'),
+            'challenge_requests.score as opponent_score',
+            'challenge_requests.status as opponent_status',
+            'cr2.status as player_status',
+            'cr2.amount_won as player_won',
+            'challenge_requests.amount_won as opponent_won'
+        )->leftJoin('challenge_requests as cr2', function ($join) {
+            $join->on('challenge_requests.session_token', '=', 'cr2.session_token');
+            $join->on('challenge_requests.user_id', '!=', 'cr2.user_id');
+        })
+        ->join('categories', 'categories.id', '=', 'challenge_requests.category_id')
+        ->groupBy('challenge_requests.session_token');
+       
     }
+
+
 
     public function columns()
     {
         return
             [
-                Column::name('request.created_at')->filterable()->label('Time Played'),
+                Column::name('challenge_requests.created_at')->filterable()->label('Time Played'),
                 Column::name('categories.name')->filterable()->label('Category'),
-                Column::callback(['request.request_status','opponent.status'], function ($request_score, $opponent_score) {
-                    if ($request_score && $opponent_score == 'COMPLETED') {
-                        return "COMPLETED";
-                    }
-                    return "ONGOING";
-                })->filterable()->filterOn(['request.request_status','opponent.status'])->label('Status'),
-
-                BooleanColumn::name('request.request_bot')->label('Player 1 Bot Status')->filterable(),
-                Column::name('request.request_username')->filterable()->label('Player 1 username'),
-                Column::name('request.request_phone_number')->filterable()->label('Player 1 Phone')->hide(),
-                Column::name('request.request_email_address')->filterable()->label('Player 1 Email')->hide(),
-                Column::name('request.request_amount_staked')->filterable()->label('Player 1 Amount Staked'),
-                Column::name('request.request_amount_won')->filterable()->label('Player 1 Amount Won'),
-                Column::name('request.request_score')->filterable()->label('Player 1 Score'),
-                Column::name('request.request_joined_on')->filterable()->label('Player 1 Joined On')->hide(),
-
-                BooleanColumn::name('user_op.is_a_bot')->label('Player 2 Bot Status')->filterable()->group('opponent'),
-                Column::name('opponent.username')->filterable()->label('Player 2 username')->group('opponent'),
-                Column::name('user_op.phone_number')->filterable()->label('Player 2 Phone')->group('opponent')->hide(),
-                Column::name('user_op.email')->filterable()->label('Player 2 Email')->group('opponent')->hide(),
-                Column::name('opponent.amount')->filterable()->label('Player 2 Amount Staked')->group('opponent'),
-                Column::name('opponent.amount_won')->filterable()->label('Player 2 Amount Won')->group('opponent'),
-                Column::name('opponent.score')->filterable()->label('Player 2 Score')->group('opponent'),
-                Column::name('user_op.created_at')->filterable()->label('Player 2 Joined On')->group('opponent')->hide(),
+                Column::name('challenge_requests.request_mode')->filterable()->label('Mode'),
+                Column::name('cr2.username')->filterable()->label('Player')->searchable(),
+                Column::name('cr2.score')->filterable()->label('Player Score')->searchable(),
+                Column::name('cr2.status')->filterable()->label('Player Status')->searchable(),
+                Column::name('cr2.amount_won')->filterable()->label('Player Won')->searchable(),
+                Column::name('challenge_requests.username')->filterable()->label('Opponent')->searchable(),
+                Column::name('challenge_requests.score')->filterable()->label('Opponent Score')->searchable(),
+                Column::name('challenge_requests.status')->filterable()->label('Opponent Status')->searchable(),
+                Column::name('challenge_requests.amount_won')->filterable()->label('Opponent Won')->searchable(),
             ];
     }
 }
