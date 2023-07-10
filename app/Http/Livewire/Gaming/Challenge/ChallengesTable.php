f4a6216978d13e5b98b3a7c9de8eee9d3bc4b01e
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-07-10 16:40:30 +0100

    refine challenges table

--- app/Http/Livewire/Gaming/Challenge/ChallengesTable.php
+++ app/Http/Livewire/Gaming/Challenge/ChallengesTable.php
@@ -6,6 +6,7 @@ use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 use Mediconesystems\LivewireDatatables\Column;
 use App\Models\Live\ChallengeRequest;
 use Illuminate\Support\Facades\DB;
+use Mediconesystems\LivewireDatatables\DateColumn;
 
 class ChallengesTable extends LivewireDatatable
 {
@@ -40,9 +41,10 @@ class ChallengesTable extends LivewireDatatable
             $join->on('challenge_requests.session_token', '=', 'cr2.session_token');
             $join->on('challenge_requests.user_id', '!=', 'cr2.user_id');
         })
-        ->join('categories', 'categories.id', '=', 'challenge_requests.category_id')
-        ->groupBy('challenge_requests.session_token');
-       
+            ->join('categories', 'categories.id', '=', 'challenge_requests.category_id')
+            ->join('users as opponent_user', 'opponent_user.id', '=', 'challenge_requests.user_id')
+            ->join('users as player_user', 'player_user.id', '=', 'cr2.user_id')
+            ->groupBy('challenge_requests.session_token');
     }
 
 
@@ -51,17 +53,29 @@ class ChallengesTable extends LivewireDatatable
     {
         return
             [
-                Column::name('challenge_requests.created_at')->filterable()->label('Time Played'),
+                DateColumn::name('challenge_requests.created_at')->filterable()->label('Time Played'),
                 Column::name('categories.name')->filterable()->label('Category'),
-                Column::name('challenge_requests.request_mode')->filterable()->label('Mode'),
-                Column::name('cr2.username')->filterable()->label('Player')->searchable(),
+                Column::callback(['cr2.user_id', 'cr2.username'], function ($id, $player) {
+                    $username = '';
+                    $id == 1 ? $username = "BOT" : $username = $player;
+                    return $username;
+                })->label('Player')->filterable(),
+
+                // Column::callbackname('cr2.username')->filterable()->label('Player')->searchable(),
                 Column::name('cr2.score')->filterable()->label('Player Score')->searchable(),
-                Column::name('cr2.status')->filterable()->label('Player Status')->searchable(),
-                Column::name('cr2.amount_won')->filterable()->label('Player Won')->searchable(),
-                Column::name('challenge_requests.username')->filterable()->label('Opponent')->searchable(),
+                Column::callback(['challenge_requests.user_id', 'challenge_requests.username'], function ($id, $opponent) {
+                    $username = '';
+                    $id == 1 ? $username = "BOT" : $username = $opponent;
+                    return $username;
+                })->label('Opponent')->filterable(),
+                // Column::name('challenge_requests.username')->filterable()->label('Opponent')->searchable(),
                 Column::name('challenge_requests.score')->filterable()->label('Opponent Score')->searchable(),
+                Column::name('cr2.status')->filterable()->label('Player Status')->searchable(),
                 Column::name('challenge_requests.status')->filterable()->label('Opponent Status')->searchable(),
+                Column::name('cr2.amount_won')->filterable()->label('Player Won')->searchable(),
                 Column::name('challenge_requests.amount_won')->filterable()->label('Opponent Won')->searchable(),
+                DateColumn::name('player_user.created_at')->filterable()->label('Player Joined On')->searchable(),
+                DateColumn::name('opponent_user.created_at')->filterable()->label('Opponent Joined On')->searchable(),
             ];
     }
 }
